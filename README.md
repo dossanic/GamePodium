9e89ef6f9a7419c933763075c4bab6f24e0361a2
dossanic <dossantosnicholas99@gmail.com>
2026-08-27 12:28:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GamePodium
+
---
+++ src/main/java/ca/nicholasdossantos/dossanic/GamePodiumApplication.java
@@ -0,0 +1,13 @@
+package ca.nicholasdossantos.dossanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePodiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePodiumApplication.class, args);
+	}
+
+}
